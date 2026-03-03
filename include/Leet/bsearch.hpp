01c8b2cacc6e64e30618e1c93a3c1e81e0a321a3
Harikeshi <sshchegolikhin@gmail.com>
2026-03-03 09:57:45 +0300

    немного изменений

--- include/Leet/bsearch.hpp
+++ include/Leet/bsearch.hpp
@@ -15,4 +15,74 @@ inline int search(std::vector<int>& nums, int target)
 
     return -1;
 }
+
+inline int guess(int mid, int target) {
+    if (mid > target)
+    {
+        return -1;
+    }
+    else if (mid < target)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+inline int guessNumber(int n, int pick) {
+	int left = 1;
+	int right = n;
+
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+
+		if (guess(mid, pick) == 0) {
+			return mid;
+		}
+		else if (guess(mid, pick) == -1) {
+			right = mid - 1;
+		}
+		else {
+			left = mid + 1;
+		}
+
+	}
+	return 0;
+}
+
+inline bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+    int left = 0, right = matrix.size();
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (target < matrix[mid][0])
+        {
+            right = mid - 1;
+        }
+        else if (target > matrix[mid][matrix[mid].size() - 1])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            // поиск внутри матрицы
+            left = 0, right = matrix[mid].size() - 1;
+            while (left <= right)
+            {
+                int _mid = left + (right - left) / 2;
+                if (matrix[mid][_mid] == target)
+                    return true;
+                else
+                    matrix[mid][_mid] > target ? right = mid - 1 : left = mid + 1;
+            }
+        }
+    }
+    return false;
+}
+
+inline int rotateSearch(std::vector<int>& nums, int target) {
+
+    return -1;
+}
 } // namespace b_search
--- tests/units/BeSearchTests.cpp
+++ tests/units/BeSearchTests.cpp
@@ -15,4 +15,54 @@ TEST(BSearchTest, Search2)
     int target = 2;
     EXPECT_TRUE(b_search::search(nums, target) == -1);
 }
+
+TEST(BSearchTest, GuessNumber1)
+{
+    EXPECT_TRUE(b_search::guessNumber(10, 6) == 6);
+}
+
+TEST(BSearchTest, GuessNumber2)
+{
+    EXPECT_TRUE(b_search::guessNumber(1, 1) == 1);
+}
+
+TEST(BSearchTest, GuessNumber3)
+{
+    EXPECT_TRUE(b_search::guessNumber(2, 1) == 1);
+}
+
+TEST(BSearchTest, SearchMatrix1)
+{
+    std::vector<std::vector<int>> matrix{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    int target = 3;
+    EXPECT_TRUE(b_search::searchMatrix(matrix, target));
+}
+
+TEST(BSearchTest, SearchMatrix2)
+{
+    std::vector<std::vector<int>>matrix {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    int target = 13;
+    EXPECT_FALSE(b_search::searchMatrix(matrix, target));
+}
+
+TEST(BSearchTest, RotateSearch1)
+{
+    std::vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
+    EXPECT_TRUE(b_search::rotateSearch(nums, target) == 4);
+}
+
+TEST(BSearchTest, RotateSearch2)
+{
+    std::vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 3;
+    EXPECT_TRUE(b_search::rotateSearch(nums, target) == -1);
+}
+
+TEST(BSearchTest, RotateSearch3)
+{
+    std::vector<int> nums = {1};
+    int target = 0;
+    EXPECT_TRUE(b_search::rotateSearch(nums, target) == -1);
+}
 } // namespace b_search
